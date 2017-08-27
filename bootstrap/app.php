0d44e4075f1ac0dae5d1bdf712b2d9fb863cede1
alooze <a.looze@gmail.com>
2017-08-27 15:11:12 +0300

    update gitignore

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -32,11 +32,11 @@ require __DIR__ . '/../routes/web.php';
  * Register base controllers
  */
 $container = $app->getContainer();
-$container['Front'] = function($container) {
-    return new \Amcms\Controllers\FrontController();
+$container['Front'] = function($c) {
+    return new \Amcms\Controllers\FrontController($c);
 };
 $container['Manager'] = function($container) {
-    return new \Amcms\Controllers\ManagerController();
+    return new \Amcms\Controllers\ManagerController($c);
 };
 
 /**
@@ -47,10 +47,23 @@ $container['Manager'] = function($container) {
 // Обработчик ошибок
 $app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware($app));
 
+// Csrf
+// $app->add(new \Slim\Csrf\Guard);
+
+// PDO
+$container['pdo'] = function ($c) {
+    $db = $c['settings']['database'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
 // Eloquent ORM
-$container['db'] = function ($container) {
+$container['db'] = function ($c) {
     $capsule = new \Illuminate\Database\Capsule\Manager;
-    $capsule->addConnection($container['settings']['database']);
+    $capsule->addConnection($c['settings']['database']);
 
     $capsule->setAsGlobal();
     $capsule->bootEloquent();
@@ -58,7 +71,18 @@ $container['db'] = function ($container) {
     return $capsule;
 };
 
+// Flash session messages
+$container['flash'] = function () {
+    return new \Slim\Flash\Messages();
+};
 
+// Monolog
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('AmcLogger');
+    $fileHandler = new \Monolog\Handler\StreamHandler("../storage/logs/app.log");
+    $logger->pushHandler($fileHandler);
+    return $logger;
+};
 
 /**
  * Finish bootstrap
